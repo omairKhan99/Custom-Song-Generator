21240e14741ea26dcd7982fbad8b664c35a8ddf5
Omair Khan <omairtkhan@gmail.com>
2024-10-04 21:39:00 -0700

    made html and main

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 from flask import Flask, render_template, request, send_file
-import openai
-from gtts import gTTS
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from lyrics_generator import generate_lyrics
+from tts_generator import generate_voice
 
